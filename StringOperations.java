566fcd75ecaf3596f3afcf5ed4512893d7f1dec7
SuvanPilla29 <69862171+SuvanPilla29@users.noreply.github.com>
2020-10-20 10:07:28 -0600

    Finished

---
+++ StringOperations.java
@@ -0,0 +1,17 @@
+public class StringOperations{
+
+    public static void main(String argos[ ]){
+  
+      //Challenge 1
+      String word ="broncos";
+      int wordLength = word.length();
+      //String firstHalf = word.substring(0,);
+  
+      //Challenge 2
+      System.out.println        
+      ("(___()'ˋ;\n/,    /ˋ");
+      
+  
+    }
+  
+  }

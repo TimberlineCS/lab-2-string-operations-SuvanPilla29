a5683ddf5dd840d759720952078f00312e1f0c69
SuvanPilla29 <69862171+SuvanPilla29@users.noreply.github.com>
2020-11-08 17:55:16 -0700

    Made Changes

--- StringOperations.java
+++ StringOperations.java
@@ -1,17 +1,19 @@
 public class StringOperations{
 
-    public static void main(String argos[ ]){
-  
-      //Challenge 1
-      String word ="broncos";
-      int wordLength = word.length();
-      //String firstHalf = word.substring(0,);
-  
-      //Challenge 2
-      System.out.println        
-      ("(___()'ˋ;\n/,    /ˋ");
-      
-  
-    }
-  
+  public static void main(String argos[ ]){
+
+    //Challenge 1
+    String word ="broncos";
+    int wordLength = word.length();
+    String firstHalf = word.substring(0,(wordLength/2));
+    String secondHalf = word.substring(wordLength/2);
+    System.out.println(secondHalf + firstHalf);
+
+    //Challenge 2
+    System.out.println        
+    ("(___()'ˋ;\n/,    /ˋ");
+    
+
   }
+
+}
